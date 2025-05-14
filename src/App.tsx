41bbf20799c178912049eb4dfb965cd7b9ae493f
daria-diahovets <daria.diahovets@gmail.com>
2025-05-14 16:31:06 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>➗️✖️➕ Calculator ➕✖️➗</h1>
+      <a
+        className="copyright"
+        target="_blank"
+        href="https://t.me/drzoidberg_portfolio"
+      >
+        @dczoidberg
+      </a>
+    </div>
+  );
+}
+
+export default App;
